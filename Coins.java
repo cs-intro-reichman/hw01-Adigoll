e3d5b3c779b8745ec416ee0292d01882cd2e3072
Adigoll <154422523+Adigoll@users.noreply.github.com>
2023-12-20 21:38:02 +0200

    Coins.java

--- Coins.java
+++ Coins.java
@@ -3,7 +3,11 @@
  * The program prints how to represent this quantity using as many quarters as possible, plus the remainder in cents.
  */
 public class Coins {
-	public static void main(String[] args) {
-		// Put your code here
+	public static void main(String[] args) 
+	{
+		int totall = Integer.parseInt(args[0]);
+		int q = totall / 25;
+		int c = totall - (q*25);
+      		System.out.println("Use " +q + " quarters and " + c + " cents");
 	}
 }
